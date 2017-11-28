408d1d61b5748b9815a04a1c04524a7a79f3ddbb
Reginaldo Alves de Barros <reginaldo.barros@fcamara.com.br>
2017-11-28 13:58:54 -0200

    add some comments

--- src/Notifications.Sample.Web/Hubs/NotificiationHub.cs
+++ src/Notifications.Sample.Web/Hubs/NotificiationHub.cs
@@ -4,6 +4,10 @@ namespace Notifications.Sample.Web.Hubs
 {
     public class NotificiationHub : Hub
     {
+        /// <summary>
+        /// https://radu-matei.com/blog/signalr-core/
+        /// </summary>
+        /// <param name="message"></param>
         public void Notify(string message)
         {
             //Clients.All.InvokeAsync("showNotifications", message);
